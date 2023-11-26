5d30e8d4834414f316cf4e84e9c94fcbd5d1ceb9
Abhiram Dasika <alfadelta10010@gmail.com>
2023-11-26 18:21:53 +0530

    added openlane design folder

---
+++ alphasoc/config.tcl
@@ -0,0 +1,8 @@
+set ::env(DESIGN_NAME) "alphasoc"
+set ::env(VERILOG_FILES) [glob $::env(DESIGN_DIR)/src/module/*.v]
+set ::env(EXTRA_LIBS) [glob $::env(DESIGN_DIR)/src/*.lib]
+set ::env(BASE_SDC_FILE) [glob $::env(DESIGN_DIR)/src/*.sdc]
+
+set ::env(CLOCK_PERIOD) "1100.00"
+set ::env(CLOCK_PORT) "clk"
+set ::env(CLOCK_NET) $::env(CLOCK_PORT)

64656ec8b5d15beef0822e0e82c6a9e9cdfa334b
Abhiram Dasika <alfadelta10010@gmail.com>
2023-12-02 17:03:38 +0530

    changes to config.tcl

--- alphasoc/config.tcl
+++ alphasoc/config.tcl
@@ -6,3 +6,5 @@ set ::env(BASE_SDC_FILE) [glob $::env(DESIGN_DIR)/src/*.sdc]
 set ::env(CLOCK_PERIOD) "1100.00"
 set ::env(CLOCK_PORT) "clk"
 set ::env(CLOCK_NET) $::env(CLOCK_PORT)
+#set ::env(PL_TARGET_DENSITY) ""
+set ::env(FP_CORE_UTIL) "80"
